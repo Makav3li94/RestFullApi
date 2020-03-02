1f7b96d6f8afff60d39e924665bb22675a1f017f
MAKAVELI <neogood@yahoo.com>
2020-03-02 12:04:33 +0330

    migrations created

--- database/migrations/2020_02_28_121322_create_categories_table.php
+++ database/migrations/2020_02_28_121322_create_categories_table.php
@@ -4,28 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+class CreateCategoriesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'categories', function ( Blueprint $table ) {
+			$table->bigIncrements( 'id' );
+			$table->string( 'name' );
+			$table->string( 'description', 1000 );
+			$table->timestamps();
+		} );
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('categories');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'categories' );
+	}
 }
--- database/migrations/2020_02_28_121335_create_products_table.php
+++ database/migrations/2020_02_28_121335_create_products_table.php
@@ -1,31 +1,38 @@
 <?php
 
+use App\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+class CreateProductsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'products', function ( Blueprint $table ) {
+			$table->bigIncrements( 'id' );
+			$table->integer( 'seller_id' );
+			$table->string( 'name' );
+			$table->string( 'description', 1000 )->nullable();
+			$table->bigInteger( 'price' );
+			$table->integer( 'quantity' )->unsigned();
+			$table->string( 'status' )->default( Product::UNAVAILABLE_PRODUCT );
+			$table->string( 'image' )->nullable();
+			$table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('products');
-    }
+			$table->foreign('seller_id')->references('id')->on('users');
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'products' );
+	}
 }
